c25322ce55b439504cbdb0854453a6562ce94e48
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2023-02-01 14:22:02 +0000

    Iteration 5

--- main.c
+++ main.c
@@ -3,12 +3,14 @@
   Average of Entered Numbers
   Max Gombos
 */
-#include <stdio.h>
+#include <stdio.h> //import functions
 
 int main(void) {
-  float num1, num2, num3, numSum;
+  float num1, num2, num3, numSum; //initialize variables
   numSum = 0; //declare numSum's starting value
-  
+
+  printf("This program finds the average of three numbers.\n");
+  //instructions ^
   printf("Please enter a number: "); //Asks user for the first number
   scanf("%f", &num1); 
   //collects the users input and formats it as a float
@@ -20,15 +22,10 @@ int main(void) {
   printf("Please enter a number: "); //Asks user for the third number
   scanf("%f", &num3); 
   //collects the users input and formats it as a float
-  
-  printf("\nFirst number: %f", num1); //prints num1
-  printf("\nSecond number: %f", num2); //prints num2
-  printf("\nThird number: %f", num3); //prints num3
 
   numSum += num1; //adds num1 to numSum
   numSum += num2; //adds num2 to numSum
   numSum += num3; //adds num3 to numSum
-  printf("\nSum: %f", numSum); //prints numSum
 
   numSum = numSum/3; //finds the average of the entered numbers
 
