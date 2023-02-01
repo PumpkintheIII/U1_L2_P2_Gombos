24f72dd6e30690921eed63cb98766e873b23bbf5
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2023-02-01 14:02:59 +0000

    Iteration 1

--- main.c
+++ main.c
@@ -6,5 +6,21 @@
 #include <stdio.h>
 
 int main(void) {
+  float num1, num2, num3, numSum;
+
+  printf("Please enter a number: "); //Asks user for the first number
+  scanf("%f", &num1); 
+  //collects the users input and formats it as a float
   
+  printf("Please enter a number: "); //Asks user for the second number
+  scanf("%f", &num2); 
+  //collects the users input and formats it as a float
+
+  printf("Please enter a number: "); //Asks user for the third number
+  scanf("%f", &num3); 
+  //collects the users input and formats it as a float
+  
+  printf("\nFirst number: %f", num1);
+  printf("\nSecond number: %f", num2);
+  printf("\nThird number: %f", num3);
 }
